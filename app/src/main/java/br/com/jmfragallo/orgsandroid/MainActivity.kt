57ee9ee6800c89038a6d10219bf762397f95a5de
Joao Marcos Fragallo De Souza <111794510+jmfragallo@users.noreply.github.com>
2024-03-15 17:58:32 -0300

    ...

--- app/src/main/java/br/com/jmfragallo/orgsandroid/MainActivity.kt
+++ app/src/main/java/br/com/jmfragallo/orgsandroid/MainActivity.kt
@@ -1,6 +1,15 @@
 package br.com.jmfragallo.orgsandroid
 
 import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val view = TextView(this)
+        view.setText("Bem Vinde")
+        setContentView(view)
+    }
 }
